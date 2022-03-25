86b76621d00848c5ea33c527c9d603414ce9caef
lubejmatej <lubejmatej@users.noreply.github.com>
2022-03-25 11:13:29 +0000

    deploy: 92479bc2642300e1140e1e8bdffcc51588d3b79e

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ModalContextProvider from "./providers/ModalProvider.js";
 import Web3ContextProvider from "./providers/Web3Provider.js";
 import "./styles.css.proxy.js";
 const App = React.lazy(() => import("./App.js"));
-var mountNode = document.getElementById("app");
+const SuspenseComponent = () => /* @__PURE__ */ React.createElement(React.Fragment, null, "...");
 ReactDOM.render(/* @__PURE__ */ React.createElement(ErrorBoundary, null, /* @__PURE__ */ React.createElement(ModalContextProvider, null, /* @__PURE__ */ React.createElement(Web3ContextProvider, null, /* @__PURE__ */ React.createElement(React.Suspense, {
-  fallback: () => /* @__PURE__ */ React.createElement(React.Fragment, null, "...")
-}, /* @__PURE__ */ React.createElement(App, null))))), mountNode);
+  fallback: /* @__PURE__ */ React.createElement(SuspenseComponent, null)
+}, /* @__PURE__ */ React.createElement(App, null))))), document.getElementById("app"));
