37d734fc6ead91b0b10f6584342f573a0b83081a
Matej Lubej <matej.lubej@hotmail.com>
2022-03-25 11:52:06 +0100

    chore: add suspense for app comp & mobile display

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import App from './App';
 import ErrorBoundary from './components/ErrorBundary/ErrorBundary';
 import ModalContextProvider from './providers/ModalProvider';
 import Web3ContextProvider from './providers/Web3Provider';
 
 import './styles.css';
 
+const App = React.lazy(() => import('./App'));
+
 var mountNode = document.getElementById('app');
 ReactDOM.render(
   <ErrorBoundary>
     <ModalContextProvider>
       <Web3ContextProvider>
-        <App />
+        <React.Suspense fallback={() => <>...</>}>
+          <App />
+        </React.Suspense>
       </Web3ContextProvider>
     </ModalContextProvider>
   </ErrorBoundary>,
