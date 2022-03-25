a05080eac31091f0d8ca2fa4e7bcd751ba86c005
lubejmatej <lubejmatej@users.noreply.github.com>
2022-03-25 10:57:28 +0000

    deploy: 201b64e96bc9b7951535e1b8f4896ebe48b1447c

--- App.css.proxy.js
+++ App.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = ".App {\n  width: 100vw;\n  height: 100vh;\n  overflow-x: hidden;\n  overflow-y: auto;\n}\n\n.App h2 {\n  text-align: center;\n  color: var(--warning);\n}\n\n.App--container {\n  max-width: 800px;\n  margin: 0 auto;\n}\n";
+  const code = ".App {\n  width: 100vw;\n  height: 100vh;\n  overflow-x: hidden;\n  overflow-y: auto;\n}\n\n.App h2 {\n  text-align: center;\n  color: var(--warning);\n}\n\n.App--container {\n  max-width: 800px;\n  margin: 0 auto;\n  padding: 0 1rem;\n}\n\n@media only screen and (min-width: 600px) {\n  .App--container {\n    padding: 0;\n  }\n}\n";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import * as React from "./_snowpack/pkg/react.js";
 import * as ReactDOM from "./_snowpack/pkg/react-dom.js";
-import App from "./App.js";
 import ErrorBoundary from "./components/ErrorBundary/ErrorBundary.js";
 import ModalContextProvider from "./providers/ModalProvider.js";
 import Web3ContextProvider from "./providers/Web3Provider.js";
 import "./styles.css.proxy.js";
+const App = React.lazy(() => import("./App.js"));
 var mountNode = document.getElementById("app");
-ReactDOM.render(/* @__PURE__ */ React.createElement(ErrorBoundary, null, /* @__PURE__ */ React.createElement(ModalContextProvider, null, /* @__PURE__ */ React.createElement(Web3ContextProvider, null, /* @__PURE__ */ React.createElement(App, null)))), mountNode);
+ReactDOM.render(/* @__PURE__ */ React.createElement(ErrorBoundary, null, /* @__PURE__ */ React.createElement(ModalContextProvider, null, /* @__PURE__ */ React.createElement(Web3ContextProvider, null, /* @__PURE__ */ React.createElement(React.Suspense, {
+  fallback: () => /* @__PURE__ */ React.createElement(React.Fragment, null, "...")
+}, /* @__PURE__ */ React.createElement(App, null))))), mountNode);
