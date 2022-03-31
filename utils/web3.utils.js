600298610dfeac15a87dfd7d9bc8fa70c3d166df
lubejmatej <lubejmatej@users.noreply.github.com>
2022-03-31 08:45:47 +0000

    deploy: c954653706fec81da0951cc3a86676a5fbd09350

--- utils/web3.utils.js
+++ utils/web3.utils.js
@@ -14,7 +14,7 @@ export class Web3Utils {
         console.log("Injected web3 detected.");
         resolve(window.web3);
       } else {
-        const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
+        const provider = new Web3.providers.HttpProvider("http://0.0.0.0:7545");
         const web3 = new Web3(provider);
         console.warn("No web3 instance injected, using default ganache instance.");
         resolve(web3);
